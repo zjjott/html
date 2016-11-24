e0a13eccbec69185022c3799eaa65fa575ea9584
zjjott <zjjott@gmail.com>
2016-11-24 19:32:36 +0800

    main and template

---
+++ main.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+from __future__ import unicode_literals
+import tornado.ioloop
+from apps.views import IndexHandler, APIHandler
+from apps.auth.urls import urls
+import apps.conf  # noq
+from tornado.options import options
+from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+from apps.core import uimodules
+from apps.core.template import FileLoader
+
+settings = {
+    "cookie_secret": options.secret,
+    "login_url": options.base_url + "user/login/",
+    "static_path": "static/dest",
+    "static_url_prefix": options.base_url + "static/",
+    "compress_response": True,
+    "debug": options.debug,
+    "ui_modules": uimodules,
+    "template_loader": FileLoader("templates/")
+}
+
+
+def make_app():
+    url1 = [
+        (r"/", IndexHandler),
+        (r"/api/", APIHandler),
+        (r"/static/(.*)", tornado.web.StaticFileHandler,
+         {"path": "static/dest"}),
+    ]
+    new_urls = url1 + urls + ec2_urls + toilet_urls
+    # print "make_app"
+    return tornado.web.Application(new_urls,
+                                   **settings)
+if __name__ == '__main__':
+    app = make_app()
+    if options.debug:
+        app.listen(options.port)
+    else:
+        # 生产环境 这样感觉有些不安😂要不要放在wsgi里面。。。ioloop怎么办。。
+        server = HTTPServer(app)
+        server.bind(options.port)
+        server.start(options.process_num)
+    IOLoop.current().start()
